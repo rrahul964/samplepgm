313774b943fc04f31700347eb0098a3e2402abcc
rrahul964 <rahul.rkrishnan964@gmail.com>
2025-07-09 12:42:59 +0530

    Update Basiccode.java

--- Basiccode.java
+++ Basiccode.java
@@ -4,17 +4,17 @@ public class Basiccode {
 	
 	public static void main(String[] args) {
 		
-		int s = 12;
-		int b = 32;
+		int a = 48;
+		int b = 50;
 		
-		System.out.println("Before swapping :"+s);
+		System.out.println("Before swapping :"+a);
 		System.out.println("Before swappin :"+b);
 		
-		s= s+b;
-		b= s-b;
-		s=s-b;
+		a= a+b;
+		b= a-b;
+		a=a-b;
 		
-		System.out.println("after swapping s="+s);
+		System.out.println("after swapping a="+a);
 		System.out.println("aftre swaping b="+b);
 	
 	
