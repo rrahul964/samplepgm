6869aace2397421dfd7ddf879fef9a088ba0a1bd
rrahul964 <rahul.rkrishnan964@gmail.com>
2025-07-09 12:40:13 +0530

    first commit

---
+++ Basiccode.java
@@ -0,0 +1,24 @@
+package com.git;
+
+public class Basiccode {
+	
+	public static void main(String[] args) {
+		
+		int s = 12;
+		int b = 32;
+		
+		System.out.println("Before swapping :"+s);
+		System.out.println("Before swappin :"+b);
+		
+		s= s+b;
+		b= s-b;
+		s=s-b;
+		
+		System.out.println("after swapping s="+s);
+		System.out.println("aftre swaping b="+b);
+	
+	
+		
+	}	
+	
+}
